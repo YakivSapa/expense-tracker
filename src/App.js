b4c18e375ffaba1be360bec6c1d7ac17a401580e
Yakiv Sapa <sapa.yakiv@gmail.com>
2023-05-04 15:36:05 +0200

    modified:   src/App.js 	modified:   src/components/Costs/Costs.js 	new file:   src/components/Costs/CostsFilter.css 	new file:   src/components/Costs/CostsFilter.js 	new file:   src/components/NewCost/CostForm.css 	new file:   src/components/NewCost/CostForm.js 	new file:   src/components/NewCost/NewCost.css 	new file:   src/components/NewCost/NewCost.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 // import CostItem from "./components/CostItem";
+import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
 
 function App() {
 
   const costs = [
     {
+      id: 'c1',
       date: new Date(2022, 2, 12),
       description: "Fridge",
       amount: 1000
     },
     {
+      id: 'c2',
       date: new Date(2022, 11, 25),
       description: "Laptop",
       amount: 1099.99
     },
     {
+      id: 'c3',
       date: new Date(2022, 11, 25),
       description: "Clothes",
       amount: 130
     },
   ];
+
+  const addCostHandler = (cost) => {
+    console.log(cost);
+    console.log('App Component');
+  };
+
   return (
     // <div>
-    //   <h1>Let's start learning React!</h1>
     //   <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount} />
     //   <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount} />
     //   <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount} />
     // </div>
     <div>
+      <NewCost onAddCost={addCostHandler} />
       <Costs costs={costs} />
     </div>
   );
